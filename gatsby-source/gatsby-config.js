a145e50380fe6798bb02c58222307fd23e320e07
Arav Agarwal <aravagarwal3073@gmail.com>
2020-06-15 12:56:16 -0400

    Adding all new files

--- gatsby-source/gatsby-config.js
+++ gatsby-source/gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
               // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
               strict: `ignore`,
               throwOnError:false,
-              displayMode: true
+              displayMode: false
             }
           },
           {
